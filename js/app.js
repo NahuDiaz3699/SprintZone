8f0be664178a4e2ce45ff756d7b2b15ebe84a7f9
NahuDiaz3699 <naahuediaz@hotmail.com>
2024-11-11 20:15:53 -0300

    avances

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-    // Selección de elementos
-    const menuButton = document.querySelector('.menuBox span');
-    const boxMenu = document.querySelector('.boxMenu');
+const precioProducto = [
+    
+]
 
-    // Función para abrir/cerrar el menú
-    menuButton.addEventListener('click', (event) => {
-        boxMenu.style.display = boxMenu.style.display === 'block' ? 'none' : 'block';
-      event.stopPropagation(); // Evita que el clic se propague al body
-    });
 
-    // Cierra el menú al hacer clic fuera de él
-    document.addEventListener('click', (event) => {
-        if (!boxMenu.contains(event.target) && event.target !== menuButton) {
-        boxMenu.style.display = 'none';
-    }
-    });
+const menuButton = document.querySelector('.material-symbols-outlined');
+const menuBox = document.querySelector('.menuBox');
+// Agregar un evento de clic al botón del menú
+menuButton.addEventListener('click', function () {
+    // Alternar la visibilidad del menú al hacer clic
+    menuBox.classList.toggle('show');
+});
+
